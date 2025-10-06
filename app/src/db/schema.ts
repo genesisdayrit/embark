ff2df37e4b8f79d52bd6d35cfc8d88180bfc1b3b
genesisdayrit <genesisdayrit@gmail.com>
2025-10-06 13:04:53 -0400

    remove test users_test table

--- app/src/db/schema.ts
+++ app/src/db/schema.ts
@@ -1,15 +1,5 @@
 import { boolean, integer, pgTable, serial, text, timestamp } from 'drizzle-orm/pg-core';
 
-export const usersTestTable = pgTable('users_test_table', {
-  id: serial('id').primaryKey(),
-  name: text('name').notNull(),
-  age: integer('age').notNull(),
-  email: text('email').notNull().unique(),
-});
-
-export type InsertUser = typeof usersTestTable.$inferInsert;
-export type SelectUser = typeof usersTestTable.$inferSelect;
-
 export const orders = pgTable('orders', {
   id: serial('id').primaryKey(),
   userId: text('user_id').notNull(),
