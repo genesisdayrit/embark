1f423089111290124f2761b4bce96e80710d8659
JY Lee <jylee.rahman@gmail.com>
2025-10-06 15:07:46 -0400

    Create signin page

--- app/src/client/App.tsx
+++ app/src/client/App.tsx
@@ -1,29 +1,12 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+import Signin from "./Signin"
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState<number>(0);
 
   return (
-    <div className="flex flex-col">
-      <p className="text-5xl">🦉</p>
-      <p className="mt-5 text-3xl">Tracking your packages ... </p>
-      <p className="text-3xl">hoot hoot</p>
-      <div className="rounded-3xl mt-15 p-5 bg-[#FAEDCD]">
-        <p className="mt-2">
-          Connect Gmail to auto-collect tracking emails.
-        </p>
-        <button className="inline-flex items-center justify-center gap-2 mt-4">
-          <img
-            src="https://developers.google.com/identity/images/g-logo.png"
-            alt="Google Logo"
-            className="w-5 h-5"
-          />
-          <span className="font-medium">Sign in with Google</span>
-        </button>
-      </div>
-    </div>
+    <Signin />
   );
 }
 
