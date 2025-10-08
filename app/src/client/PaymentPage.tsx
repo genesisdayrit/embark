886e2047ca51951823a1adfecf750e2863068d8c
JY Lee <jylee.rahman@gmail.com>
2025-10-08 16:54:09 -0400

    Edit few details

--- app/src/client/PaymentPage.tsx
+++ app/src/client/PaymentPage.tsx
@@ -5,18 +5,17 @@ import { Button } from "../components/ui/button"
 function Payment() {
 
     return (
-        <div className="bg-[#FFFFFF] rounded-2xl p-10 w-140 text-2xl flex flex-col gap-8 border border-gray-200 shadow-lg">
+        <div className="bg-[#FFFFFF] rounded-4xl p-10 w-140 text-2xl flex flex-col gap-5 border border-gray-200 items-start shadow-lg">
             <p>$0 for 1 month</p>
             <p className="text-4xl font-extrabold">Premium</p>
-            <p className="text-4xl font-bold ">$0 for 1 month</p>
+            <p className="text-4xl font-bold">$0 for 1 month</p>
             <p>$4.99 / month after</p>
 
             <ul className="font-bold">
-                <li>1 Premium account</li>
                 <li> Cancel anytime</li>
             </ul>
 
-            <Button className="text-2xl p-10">Try 1 month for $0</Button>
+            <button className="text-2xl p-5 font-bold rounded-4xl bg-[#CCD5AE] w-full">Try 1 month for $0</button>
 
             <p className="text-lg text-gray-700">
                 15 hours/month of listening time from our audiobooks subscriber catalog
