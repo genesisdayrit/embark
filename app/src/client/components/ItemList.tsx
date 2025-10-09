ebf0b24510f98d5f9e7cfc78d31ab8530ee8e182
JY Lee <jylee.rahman@gmail.com>
2025-10-09 12:34:11 -0400

    Resize Dashboard

--- app/src/client/components/ItemList.tsx
+++ app/src/client/components/ItemList.tsx
@@ -15,7 +15,7 @@ function ItemList({ order }: ItemListProps) {
             <div className="relative w-full bg-[#FAEDCD] rounded-xl p-4 justify-start flex gap-5 h-70">
                 <p className="relative border-2 rounded-xl w-100">item img</p>
                 <div className="relative w-full flex flex-col items-start gap-3 justify-center">
-                    <p className="font-bold text-2xl">Arriving on: {order.estimatedDeliveryDate?.toLocaleString()}</p>
+                    <p className="font-bold text-xl">Arriving on: {order.estimatedDeliveryDate?.toLocaleString()}</p>
                     <p>item mercant: {order.merchant}</p>
                     <p>tracking number: {order.trackingNumbers} </p>
                 </div>
