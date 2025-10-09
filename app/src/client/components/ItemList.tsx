ca38d82448f4f8dc2d31edcbc74bfcb6a923b8fb
JY Lee <jylee.rahman@gmail.com>
2025-10-09 11:20:12 -0400

    Edit Dashboard styling

--- app/src/client/components/ItemList.tsx
+++ app/src/client/components/ItemList.tsx
@@ -12,7 +12,7 @@ function ItemList({ order }: ItemListProps) {
 
     return (
         <>
-            <div className="relative w-[80%] bg-[#FAEDCD] rounded-xl p-4 justify-start flex gap-5 h-70">
+            <div className="relative w-full bg-[#FAEDCD] rounded-xl p-4 justify-start flex gap-5 h-70">
                 <p className="relative border-2 rounded-xl w-100">item img</p>
                 <div className="relative w-full flex flex-col items-start gap-3 justify-center">
                     <p className="font-bold text-2xl">Arriving on: {order.estimatedDeliveryDate?.toLocaleString()}</p>
