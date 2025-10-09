7fd46b5b61a777d3a8c183ae984e3b73abaa34ba
JY Lee <jylee.rahman@gmail.com>
2025-10-09 16:28:27 -0400

    Resolve merge conflict on Itemlist

--- app/src/client/components/ItemList.tsx
+++ app/src/client/components/ItemList.tsx
@@ -15,29 +15,18 @@ function ItemList({ order }: ItemListProps) {
                 <div className="relative w-full flex flex-col items-start justify-center text-base">
                     <p className="font-bold">Tracking #: {order.trackingNumbers}</p>
                     <p className="border border-1 w-full mt-2 mb-5"></p>
-                    <p>Status: </p>
-                    <p>Arriving on: {order.estimatedDeliveryDate?.toLocaleString()}</p>
+                    <p>Status: {getOrderStatus(order)}</p>
+                    <p>Estimated Delivery Date: {formatDate(order.estimatedDeliveryDate)}</p>
                     <p>Item Merchant: {order.merchant}</p>
                     <p className="border border-1 w-full mt-5"></p>
                 </div>
 
                 <div className="flex text-base">
-                    <p className="relative border border-2 rounded-xl w-[40%] h-30">item img</p>
+                    <p className="relative border border-2 rounded-xl w-[30%] h-30">item img</p>
                     <div className="flex flex-col justify-start items-start">
-                        <p className="ml-10 font-bold">product name goes here</p>
-                        <p className="ml-10">product</p>
-                        <p className="ml-10">details</p>
-                        <p className="ml-10">maybe?</p>
+                        <p className="ml-10">Tracking #: {order.trackingUrls}</p>
                     </div>
 
-            <div className="relative w-full bg-[#D4A373] rounded-xl p-4 justify-start flex gap-10 h-55 text-xl">
-                <p className="relative border-2 rounded-xl w-100">item img</p>
-                <div className="relative w-full flex flex-col items-start gap-3 justify-center">
-                    <p className="font-bold">Arriving on: {formatDate(order.estimatedDeliveryDate)}</p>
-                    <p>item mercant: {order.merchant}</p>
-                    <p>tracking number: {order.trackingNumbers} </p>
-                    <p>order date: {formatDate(order.orderDate)}</p>
-                    <p>order status: <span className="font-semibold">{getOrderStatus(order)}</span></p>
                 </div>
             </div>
         </>
